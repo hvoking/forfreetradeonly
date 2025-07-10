0ce49509f646b9a574138b70a8513082e94a4799
hvoking <gus@ugeom.com>
2025-07-10 19:23:35 -0300

    testind without signals

--- src/app/layout/viewer/mask/index.tsx
+++ src/app/layout/viewer/mask/index.tsx
@@ -8,27 +8,34 @@ import { Geometry } from './geometry';
 
 // Context imports
 import { useMapboxIsochroneApi } from 'context/mapbox/isochrone';
+import { useLayer } from 'context/layer';
+import { useMarkers } from 'context/markers';
 
 // Third party imports
 import * as turf from '@turf/turf';
 
 export const Mask = ({ marker }: any) => {
   const { fetchIsochrone } = useMapboxIsochroneApi();
+  const { getGeojson } = useLayer();
+  const { updateMarkers } = useMarkers();
+
+  const { id, center, radius, geometryType, data } = marker; 
+  const { lng, lat } = center;
 
   const [ boundary, setBoundary ] = useState<any>(null);
-
-  const { id, center, radius, geometryType } = marker; 
-  const { lng, lat } = center;
 
   useEffect(() => {
     const fetchBoundary = async (marker: any) => {
       if (geometryType === 'iso') {
         const data = await fetchIsochrone(marker);
-        setBoundary(data.features[0]);
+        const currentBoundary = data.features[0]
+        setBoundary(currentBoundary);
+        updateMarkers(id, 'data', getGeojson(currentBoundary, 'LineString', 'road'));
       } 
       else {
         const circle = turf.circle([ lng, lat ], radius);
         setBoundary(circle);
+        updateMarkers(id, 'data', getGeojson(circle, 'LineString', 'road'));
       }
     };
     fetchBoundary(marker);
